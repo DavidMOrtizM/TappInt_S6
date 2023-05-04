a2b1590fcd485d77cf08d20ab3b225c83cab79cf
dmortiz <dmortiz1@gmail.com>
2023-05-04 16:41:47 -0400

    PHP

--- modelos/ClaseUsuario.php
+++ modelos/ClaseUsuario.php
@@ -1,55 +1,74 @@
 <?php
 class Usuario {
-  private $username;
-  private $password;
-  private $logged_in;
-  private $session_map;
-
-  function __construct() {
-    $this->username = '';
-    $this->password = '';
-    $this->logged_in = false;
-    $this->session_map = array();
-  }
-
-  function autenticar($username, $password) {
-    // Aquí iría la lógica de autenticación, por ejemplo, consultar una base de datos de usuarios
-    // Si la autenticación es exitosa, se asignan los valores de $username y $password
-//    if (/* autenticación exitosa */) {
-      $this->username = $username;
-      $this->password = $password;
-      $this->logged_in = true;
-//      return true;
-//    } else {
-//      return false;
-//    }
-  }
-
-  function registrar($username, $password) {
-    // Aquí iría la lógica de registro, por ejemplo, insertar un nuevo usuario en una base de datos
-    // Si el registro es exitoso, se asignan los valores de $username y $password
-//    if (/* registro exitoso */) {
-      $this->username = $username;
-      $this->password = $password;
-      $this->logged_in = true;
- //     return true;
- //   } else {
- //     return false;
- //   }
-  }
-
-  function guardar_sesion($key, $value) {
-    // Aquí se guarda la sesión en el mapa de sesiones
-    $this->session_map[$key] = $value;
-  }
-
-  function recuperar_sesion($key) {
-    // Aquí se recupera la sesión del mapa de sesiones
-    if (isset($this->session_map[$key])) {
-      return $this->session_map[$key];
-    } else {
-      return null;
+    private $username;
+    private $password;
+    private $logged_in;
+    private $session_map;
+  
+    function __construct() {
+      $this->username = '';
+      $this->password = '';
+      $this->logged_in = false;
+      $this->session_map = array();
+    }
+  
+    function autenticar($username, $password) {
+      $usuarios = $this->obtener_usuarios();
+      foreach ($usuarios as $usuario) {
+        if ($usuario->username === $username && $usuario->password === $password) {
+          $this->username = $username;
+          $this->password = $password;
+          $this->logged_in = true;
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    function registrar($username, $password) {
+      if ($this->usuario_existe($username)) {
+        // El usuario ya existe, no se puede registrar
+        return false;
+      } else {
+        // El usuario no existe, se puede registrar
+        // Aquí iría la lógica para insertar el nuevo usuario en la base de datos
+        $this->username = $username;
+        $this->password = $password;
+        $this->logged_in = true;
+        return true;
+      }
+    }
+  
+    function guardar_sesion($key, $value) {
+      // Aquí se guarda la sesión en el mapa de sesiones
+      $this->session_map[$key] = $value;
+    }
+  
+    function recuperar_sesion($key) {
+      // Aquí se recupera la sesión del mapa de sesiones
+      if (isset($this->session_map[$key])) {
+        return $this->session_map[$key];
+      } else {
+        return null;
+      }
+    }
+  
+    function usuario_existe($username) {
+      $usuarios = $this->obtener_usuarios();
+      foreach ($usuarios as $usuario) {
+        if ($usuario->username === $username) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    function obtener_usuarios() {
+      $usuarios_json = file_get_contents("modelos/usuarios.json");
+      $usuarios = json_decode($usuarios_json);
+      return $usuarios;
     }
   }
-}
+
+
 ?>
